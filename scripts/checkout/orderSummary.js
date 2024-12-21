4c55e7dbef5fec34466fad3702187841f94e5278
GanYoYo <1197445586@qq.com>
2024-12-21 16:56:20 +0800

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,11 +3,7 @@ import { getProduct, products } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import { deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
-
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -136,10 +132,12 @@ export function renderOrderSummary() {
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
+            renderPaymentSummary();
 
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
             container.remove();
+            renderPaymentSummary();
         })
     });
 
@@ -152,6 +150,7 @@ export function renderOrderSummary() {
 
                 updateDeliveryOption(productId, deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             })
         });
 

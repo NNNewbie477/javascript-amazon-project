55e74e4d17c8c512fff3cd3020c9578bb3e6a913
GanYoYo <1197445586@qq.com>
2024-12-26 18:38:54 +0800

    use inheritance and poly for product.js

--- data/products.js
+++ data/products.js
@@ -36,7 +36,32 @@ class Product {
     return `${formatCurrency(this.priceCents)}`
   }
 
+  extraInfoHTML() {
+    return '';
+  }
+
 }
+
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink; 
+  }
+
+  extraInfoHTML() {
+    super.extraInfoHTML();
+
+    return `
+    <a href="${this.sizeChartLink}">
+    Size chart
+    </a>
+    `;
+  }
+}
+
 
 
 export const products = [
@@ -699,7 +724,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
-console.log(products)
